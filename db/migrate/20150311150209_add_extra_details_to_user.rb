14a663f49544b77d33ce72e82526c954b39fabb2
palmbeachjeff <palmbeachjeff@gmail.com>
2015-03-11 11:06:23 -0400

    Added Stripe Gem/migrate db using Stripe documentation

---
+++ db/migrate/20150311150209_add_extra_details_to_user.rb
@@ -0,0 +1,7 @@
+class AddExtraDetailsToUser < ActiveRecord::Migration
+  
+  def change
+    add_column :users, :subscribed, :boolean, :default => false
+    add_column :users, :stripeid, :string
+  end
+end
