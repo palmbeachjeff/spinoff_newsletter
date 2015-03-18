49175f7a7577ccfd2ce5f3ef6444bdb3c90f5d1a
Andell <Andell@Andell.local>
2015-03-18 01:35:12 -0400

    fix routes and create basic layout for default page when user login

--- app/controllers/spinoffs_controller.rb
+++ app/controllers/spinoffs_controller.rb
@@ -1,7 +1,12 @@
 class SpinoffsController < ApplicationController
+  before_filter :authenticate_user!
 
   def index
     @spinoffs = Spinoff.paginate(per_page: 25, page: params[:page]) 
+  end
+
+  def show
+    @spinoff = Spinoff.find(params[:id])
   end
 
 end
