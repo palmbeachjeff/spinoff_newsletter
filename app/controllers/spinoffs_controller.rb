f03c3b4ebf4082eb2463f26d5f4b4cfe20e6b26c
palmbeachjeff <palmbeachjeff@gmail.com>
2015-03-20 12:46:37 -0400

    set up ActiveAdmin Dashboard and set admin email addresses

--- app/controllers/spinoffs_controller.rb
+++ app/controllers/spinoffs_controller.rb
@@ -1,17 +1,22 @@
 class SpinoffsController < ApplicationController
   before_filter :authenticate_user!
 
+
+
   def index
     @spinoffs = Spinoff.paginate(per_page: 25, page: params[:page]) 
   end
 
   def show
-    @spinoff = Spinoff.find(params[:id])
+      @spinoff = Spinoff.find(params[:id])
   end
-
+  
   def admin
+  
   end
   
 
+
 end
+
 
