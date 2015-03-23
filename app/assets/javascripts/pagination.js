66a9a4f9eab0cf98b3770c56e750083f8c4373ee
Andell <Andell@Andell.local>
2015-03-23 19:30:15 -0400

    add AJAX, additional into to spinoff show page and charts to display close data for the past 5 years

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,3 +1,10 @@
 $(function(){
-  // alert('DOM is loaded!');
+  $('#spinoffs').on('click', '.pagination > a', function(){
+    $.get(this.href, null, null, "script");
+    return false;
+  });
 });
+
+// min 1 should be into (intro to you and project- title of app, what it does and why you built it)
+// min 2 - 4 should be your tech stack with live demo or slides (what it actually does and how)
+// outro - What you've learned as a developer throughout the course
