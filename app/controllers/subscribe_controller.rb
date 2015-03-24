efb2374009aaa2499728458c25f65d845fa9c779
palmbeachjeff <palmbeachjeff@gmail.com>
2015-03-24 16:57:38 -0400

    removed mailer due to heroku error

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -23,22 +23,4 @@ class SubscribeController < ApplicationController
 
       redirect_to spinoffs_index_path, :notice => "Your subscription was setup! You may now access the Spin-off subscription list!"
   end
-
-  def create
-    @current_user.subscribed = current_user.subscribed.new(params[:current_user.subscribed])
- 
-    respond_to do |format|
-      if current_current_user.subscribed.save
-        # Tell the current_user.subscribedMailer to send a welcome email after save
-        current_user.subscribedMailer.welcome_email(@current_user.subscribed).deliver_later
- 
-        format.html { redirect_to(@current_user.subscribed, notice: 'current_user.subscribed was successfully created.') }
-        format.json { render json: @current_user.subscribed, status: :created, location: @current_user.subscribed }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @current_user.subscribed.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
 end
